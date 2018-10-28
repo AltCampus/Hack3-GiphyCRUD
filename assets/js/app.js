89956db4fda4d991f503a9fe7e4000b26754d1f0
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-10-28 14:52:24 +0530

    bolierplate and fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,14 @@
 var search = document.querySelector(".user-search");
-var uri = "http://api.giphy.com/v1/gifs/search?";
+var url = "http://api.giphy.com/v1/gifs/search?";
 var key = "&api_key=YmbYIzqV3QRKpdYOkG2FarNnE0pDClSS";
 var gif1 = document.querySelector('.gif1');
-var gif2 = document.querySelector('.gif2');
-var gif3 = document.querySelector('.gif3');
-var gif4 = document.querySelector('.gif4');
-var gif5 = document.querySelector('.gif5');
-var gif6 = document.querySelector('.gif6');
 var address = document.querySelector('.address');
-var displayGif = document.querySelector(".display-gif");
-var spinner = document.querySelector(".lds-hourglass");
-spinner.style.display = "none";
 
 function displayInfo(userSearch){
-  var completeUrl =  uri + key + query;
-  fetch(completeUrl).then(sata =>  sata.json()).then(res => {
-  //   res.data.forEach((item, i) => {
-
-  //   });
-  //  for(var i = 0 ; i < res.data.length ; i++){
+  var completeUrl =  url + key + query;
+  fetch(completeUrl).then(data =>  data.json()).then(res => {
     gif1.src = res.data[0].images.fixed_height_downsampled.url;
-  // }
-    gif2.src = res.data[1].images.fixed_height_downsampled.url;
-    gif3.src = res.data[2].images.fixed_height_downsampled.url;
-    gif4.src = res.data[3].images.fixed_height_downsampled.url;
-    gif5.src = res.data[4].images.fixed_height_downsampled.url;
-    gif6.src = res.data[5].images.fixed_height_downsampled.url;
     search.value = "";
-    spinner.style.display = "none";
   });
 }
 
@@ -37,7 +18,6 @@ function displayValue(event){
   query = `&q=${userSearch}`;
   if(event.code == "Enter") {
   displayInfo(userSearch);
-  spinner.style.display = "block";
 }
 }
 
