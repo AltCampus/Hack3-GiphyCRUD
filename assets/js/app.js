17b19298eb8757694bf7e675aaef428beccb4aff
mammothgazillionaire <mammothgazillionaire@gmail.com>
2018-10-28 17:03:23 +0530

    random trending

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,18 @@ var search = document.querySelector(".user-search");
 var url = "http://api.giphy.com/v1/gifs/search?";
 var key = "&api_key=YmbYIzqV3QRKpdYOkG2FarNnE0pDClSS";
 var gif1 = document.querySelector('.gif1');
-var address = document.querySelector('.address');
+var random = "random";
+var trending = "trending";
+var randomBtn = document.getElementById("random");
+var trendingBtn = document.getElementById("trending");
 
-function displayInfo(userSearch){
+function displayInfo(){
   var completeUrl =  url + key + query;
   fetch(completeUrl).then(data =>  data.json()).then(res => {
     gif1.src = res.data[0].images.fixed_height_downsampled.url;
     search.value = "";
   });
 }
-
 
 function displayValue(event){
   var userSearch = search.value;
@@ -21,4 +23,16 @@ function displayValue(event){
 }
 }
 
+function displayRandom(){
+  
+}
+
+
+function displayTrending(){
+  
+}
+
+
 search.addEventListener('keypress', displayValue);
+randomBtn.addEventListener(onclick, displayRandom);
+trendingBtn.addEventListener(onclick,displayTrending);
